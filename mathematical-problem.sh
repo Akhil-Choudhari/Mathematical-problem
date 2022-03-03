300821a5b74e46859379cd023b290de9b8496f44
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-03-03 20:27:59 +0530

    multiplication of above two values

--- mathematical-problem.sh
+++ mathematical-problem.sh
@@ -6,4 +6,6 @@ z=$(($x + $y))
 echo  "$z =is the addition of two above number";
 z=$(($x - $y))
 echo "$z =is the subtraction of above two numbers";
+z=$(($x * $y))
+echo "$z =is the multiplication of above two numbers";
 
